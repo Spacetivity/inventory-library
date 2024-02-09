dfeac8d293708bc34f1318a7cb3323c8e915d3d7
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2024-02-09 21:48:00 +0100

    Implemented bukkit extension for inventory api

--- inventory-api/src/main/kotlin/net/spacetivity/inventory/api/extension/BukkitInventoryExtension.kt
+++ inventory-api/src/main/kotlin/net/spacetivity/inventory/api/extension/BukkitInventoryExtension.kt
@@ -4,33 +4,33 @@ import net.kyori.adventure.text.Component
 import net.spacetivity.inventory.api.SpaceInventoryProvider
 import net.spacetivity.inventory.api.inventory.InventoryProvider
 import net.spacetivity.inventory.api.inventory.SpaceInventory
-import org.bukkit.Bukkit
+import org.bukkit.Server
 import org.bukkit.entity.Player
 
-fun Bukkit.openStaticInventory(holder: Player, title: Component, provider: InventoryProvider) {
+fun Server.openStaticInventory(holder: Player, title: Component, provider: InventoryProvider) {
     SpaceInventoryProvider.api.inventoryHandler.openStaticInventory(holder, title, provider, true)
 }
 
-fun Bukkit.openInventory(holder: Player, key: String) {
+fun Server.openInventory(holder: Player, key: String) {
     SpaceInventoryProvider.api.inventoryHandler.getInventory(holder, key)?.open(holder)
 }
 
-fun Bukkit.getInventory(holder: Player, key: String): SpaceInventory? {
+fun Server.getInventory(holder: Player, key: String): SpaceInventory? {
     return SpaceInventoryProvider.api.inventoryHandler.getInventory(holder, key)
 }
 
-fun Bukkit.cacheInventory(holder: Player, title: Component, provider: InventoryProvider) {
+fun Server.cacheInventory(holder: Player, title: Component, provider: InventoryProvider) {
     SpaceInventoryProvider.api.inventoryHandler.cacheInventory(holder, title, provider)
 }
 
-fun Bukkit.removeCachedInventory(holder: Player, inventory: SpaceInventory) {
+fun Server.removeCachedInventory(holder: Player, inventory: SpaceInventory) {
     SpaceInventoryProvider.api.inventoryHandler.removeCachedInventory(holder, inventory)
 }
 
-fun Bukkit.clearCachedInventories(holder: Player) {
+fun Server.clearCachedInventories(holder: Player) {
     SpaceInventoryProvider.api.inventoryHandler.clearCachedInventories(holder)
 }
 
-fun Bukkit.updateCachedInventory(holder: Player, key: String) {
+fun Server.updateCachedInventory(holder: Player, key: String) {
     SpaceInventoryProvider.api.inventoryHandler.updateCachedInventory(holder, key)
 }
