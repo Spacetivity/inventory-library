cb0b862365bcd63fb8126cbb6ca8fc65dcfa30fa
Tobias Heimboeck <tobiasheimboeck@outlook.com>
2025-02-27 16:37:54 +0100

    fix: removed unnecessary import

--- inventory-api/src/main/kotlin/net/spacetivity/inventory/api/item/InventoryPos.kt
+++ inventory-api/src/main/kotlin/net/spacetivity/inventory/api/item/InventoryPos.kt
@@ -3,8 +3,8 @@ package net.spacetivity.inventory.api.item
 data class InventoryPos(val row: Int, val column: Int) {
 
     companion object {
-        fun of(row: Int, column: Int): net.spacetivity.inventory.api.item.InventoryPos {
-            return net.spacetivity.inventory.api.item.InventoryPos(row, column)
+        fun of(row: Int, column: Int): InventoryPos {
+            return InventoryPos(row, column)
         }
     }
 
